9e09245311e2c90a5dafcb5c643f1f67e4c3641a
Ubuntu <ubuntu@ip-172-31-49-42.ec2.internal>
2023-03-18 16:48:04 +0000

    add Build file

---
+++ build/precache-manifest.49ddc074ebaf00df84f3333b6fc98e51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5685dca8e3b7c44967b3b9e4a29cb39e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf99a4a7069152580893",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9ae4eda03ebfa6c2d356",
+    "url": "/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "bf99a4a7069152580893",
+    "url": "/static/js/2.db21659b.chunk.js"
+  },
+  {
+    "revision": "9ae4eda03ebfa6c2d356",
+    "url": "/static/js/main.24d0a016.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
